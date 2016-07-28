cc36bec67999b6fba79f2e74a5b33d6dbae40cb9
Prelang Builder <builder@prelang.com>
2016-07-28 15:51:53 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Artifact' to allow for create, update

--- app/admin/artifact.rb
+++ app/admin/artifact.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Artifact do
+  permit_params :subject, :description, :type, :status
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
